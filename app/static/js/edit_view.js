a847a7a7e136f976fe348c872b993923bb0684c8
Samuel Milette-Lacombe <samuel.milette-lacombe@savoirfairelinux.com>
2013-07-03 15:34:35 -0400

    - The columns editing in view edit page is now working. It is possible to drag drop them to change column display order.

--- app/static/js/edit_view.js
+++ app/static/js/edit_view.js
@@ -4,8 +4,17 @@ $(document).ready(function () {
     });
 
     $(".removeCol").click(function() {
-        $(this).parent('div').parent('div').remove();
+        $(this).parent('div').parent('div').parent('.clonable-col').remove();
+        reorder_columns();
     });
+
+     $( "#sortable" ).sortable({
+placeholder: "ui-state-highlight",
+update: function (e, ui) {
+           reorder_columns() 
+        }
+});
+
 });
 
 function clone_field_list(selector) {
@@ -24,4 +33,16 @@ function clone_field_list(selector) {
     $(selector).after(new_element);
 }
 
+function reorder_columns() {
+    $('.clonable-col').each(function( index )  {
+        var selectCol = $(this).find('select');
+        var labelCol = $(this).find('label');
+        
+        var old_id = selectCol.attr('id');
+        var old_num = parseInt(old_id.replace(/.*-(\d{1,4})-.*/m, '$1')); 
 
+        var new_id = old_id.replace('-' + (old_num) + '-', '-' + index + '-');
+        selectCol.attr({'name': new_id, 'id': new_id});
+        labelCol.attr('for', new_id);
+    });
+}
