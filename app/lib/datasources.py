1d6a8db7c802135c47fbb1106c79d72359ceebda
Samuel Milette-Lacombe <samuel.milette-lacombe@savoirfairelinux.com>
2013-07-08 17:57:11 -0400

    Add check_mk library files related to livestatus query for views.

---
+++ app/lib/datasources.py
@@ -0,0 +1,203 @@
+#!/usr/bin/python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+# +------------------------------------------------------------------+
+# |             ____ _               _        __  __ _  __           |
+# |            / ___| |__   ___  ___| | __   |  \/  | |/ /           |
+# |           | |   | '_ \ / _ \/ __| |/ /   | |\/| | ' /            |
+# |           | |___| | | |  __/ (__|   <    | |  | | . \            |
+# |            \____|_| |_|\___|\___|_|\_\___|_|  |_|_|\_\           |
+# |                                                                  |
+# | Copyright Mathias Kettner 2012             mk@mathias-kettner.de |
+# +------------------------------------------------------------------+
+#
+# This file is part of Check_MK.
+# The official homepage is at http://mathias-kettner.de/check_mk.
+#
+# check_mk is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2.  check_mk is  distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+# ails.  You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+
+##################################################################################
+# Data sources
+##################################################################################
+# title: Used as display-string for the datasource in multisite (e.g. view editor)
+#
+# table: Might be a string which refers to a livestatus table or a function
+#        which is called instead of the livestatus function query_data().
+#
+# infos: A key to be used to create groups out of single painters and filters.
+#        e.g. 'host' groups all painters and filters which begin with "host_".
+#        Out of this declaration multisite knows which filters or painters are
+#        available for the single datasources.
+#
+# merge_by:
+#  1. Results in fetching these columns from the datasource.
+#  2. Rows from different sites are merged together. For example members 
+#     of hostgroups which exist on different sites are merged together to
+#     show the user one big hostgroup.
+#
+# add_columns: list of columns the datasource is known to add itself
+#
+# add_headers: additional livestatus headers to add to each call
+#
+# keys: columns which must be fetched in order to execute commands on 
+# the items (= in order to identify the items and gather all information
+# needed for constructing Nagios commands)
+# those columns are always fetched from the datasource for each item
+#
+# idkeys: these are used to generate a key which is uniq for each data row
+# is used to identify an item between http requests
+#
+# join: A view can display e.g. host-rows and include information from e.g.
+#       the service table to create a column which shows e.g. the state of one
+#       service.
+#       With this attibute it is configured which tables can be joined into 
+#       this table and by which attribute. It must be given as tuple, while
+#       the first argument is the name of the table to be joined and the second
+#       argument is the column in the master table (in this case hosts) which
+#       is used to match the rows of the master and slave table.
+#
+# joinkey: Each joined column in the view can have a 4th attribute which is
+#          used as value for this column to filter the datasource query
+#          to get the matching row of the slave table.
+#
+# ignore_limit: Ignore the soft/hard query limits in view.py/query_data(). This
+#               fixes stats queries on e.g. the log table.
+
+multisite_datasources["hosts"] = {
+    "title"   : _("All hosts"),
+    "table"   : "hosts",
+    "infos"   : [ "host" ],
+    "keys"    : [ "host_name", "host_downtimes" ],
+    "join"    : ( "services", "host_name" ),
+    "idkeys"  : [ "site", "host_name" ],
+}
+
+multisite_datasources["hostsbygroup"] = {
+    "title"   : _("Hosts grouped by host groups"),
+    "table"   : "hostsbygroup",
+    "infos"   : [ "host", "hostgroup" ],
+    "keys"    : [ "host_name", "host_downtimes" ],
+    "join"    : ( "services", "host_name" ),
+    "idkeys"  : [ "site", "hostgroup_name", "host_name" ],
+}
+
+multisite_datasources["services"] = {
+    "title"   : _("All services"),
+    "table"   : "services",
+    "infos"   : [ "service", "host" ],
+    "keys"    : [ "host_name", "service_description", "service_downtimes" ],
+    "joinkey" : "service_description",
+    "idkeys"  : [ "site", "host_name", "service_description" ],
+}
+
+multisite_datasources["servicesbygroup"] = {
+    "title"   : _("Services grouped by service groups"),
+    "table"   : "servicesbygroup",
+    "infos"   : [ "service", "host", "servicegroup" ],
+    "keys"    : [ "host_name", "service_description", "service_downtimes" ],
+    "idkeys"  : [ "site", "servicegroup_name", "host_name", "service_description" ],
+}
+
+multisite_datasources["servicesbyhostgroup"] = {
+    "title"   : _("Services grouped by host groups"),
+    "table"   : "servicesbyhostgroup",
+    "infos"   : [ "service", "host", "hostgroup" ],
+    "keys"    : [ "host_name", "service_description", "service_downtimes" ],
+    "idkeys"  : [ "site", "hostgroup_name", "host_name", "service_description" ],
+}
+
+multisite_datasources["hostgroups"] = {
+    "title"   : _("Hostgroups"),
+    "table"   : "hostgroups",
+    "infos"   : [ "hostgroup" ],
+    "keys"    : [ "hostgroup_name" ],
+    "idkeys"  : [ "site", "hostgroup_name" ],
+}
+
+multisite_datasources["merged_hostgroups"] = {
+    "title"    : _("Hostgroups, merged"),
+    "table"    : "hostgroups",
+    "merge_by" : "hostgroup_name",
+    "infos"    : [ "hostgroup" ],
+    "keys"     : [ "hostgroup_name" ],
+    "idkeys"   : [ "hostgroup_name" ],
+}
+
+multisite_datasources["servicegroups"] = {
+    "title"    : _("Servicegroups"),
+    "table"    : "servicegroups",
+    "infos"    : [ "servicegroup" ],
+    "keys"     : [ "servicegroup_name" ],
+    "idkeys"   : [ "site", "servicegroup_name" ],
+}
+
+multisite_datasources["merged_servicegroups"] = {
+    "title"    : _("Servicegroups, merged"),
+    "table"    : "servicegroups",
+    "merge_by" : "servicegroup_name",
+    "infos"    : [ "servicegroup" ],
+    "keys"     : [ "servicegroup_name" ],
+    "idkeys"   : [ "servicegroup_name" ],
+}
+
+multisite_datasources["comments"] = {
+    "title"    : _("Host- and Servicecomments"),
+    "table"    : "comments",
+    "infos"    : [ "comment", "host", "service" ],
+    "keys"     : [ "comment_id", "comment_type" ],
+    "idkeys"   : [ "comment_id" ],
+}
+
+multisite_datasources["downtimes"] = {
+    "title"    : _("Schedules Downtimes"),
+    "table"    : "downtimes",
+    "infos"    : [ "downtime", "host", "service" ],
+    "keys"     : [ "downtime_id", "service_description" ],
+    "idkeys"   : [ "downtime_id" ],
+}
+
+multisite_datasources["log"] = {
+    "title"    : _("The Logfile"),
+    "table"    : "log",
+    "infos"    : [ "log", "host", "service", "contact", "command" ],
+    "keys"     : [],
+    "idkeys"   : [ "log_lineno" ],
+}
+
+multisite_datasources["log_events"] = {
+    "title"       : _("Host and Service Events"),
+    "table"       : "log",
+    "add_headers" : "Filter: class = 1\nFilter: class = 3\nOr: 2\n",
+    "infos"       : [ "log", "host", "service" ],
+    "keys"        : [],
+    "idkeys"      : [ "log_lineno" ],
+}
+
+multisite_datasources["log_host_events"] = {
+    "title"       : _("Host Events"),
+    "table"       : "log",
+    "add_headers" : "Filter: class = 1\nFilter: class = 3\nOr: 2\nFilter: service_description = \n",
+    "infos"       : [ "log", "host" ],
+    "keys"        : [],
+    "idkeys"      : [ "log_lineno" ],
+}
+
+multisite_datasources["alert_stats"] = {
+    "title"        : _("Alert Statistics"),
+    "table"        : "log",
+    "add_headers"  : "Filter: class = 1\nStats: state = 0\nStats: state = 1\nStats: state = 2\nStats: state = 3\nStats: state != 0\n",
+    "add_columns"  : [ "alerts_ok", "alerts_warn", "alerts_crit", "alerts_unknown", "alerts_problem" ],
+    "infos"        : [ "log", "host", "service", "contact", "command" ],
+    "keys"         : [],
+    "idkeys"       : [ 'host_name', 'service_description' ],
+    "ignore_limit" : True,
+}
---
+++ app/lib/livestatus_query.py
@@ -0,0 +1,107 @@
+from app.lib.livestatusconnection import live
+
+# Retrieve data via livestatus, convert into list of dicts,
+# prepare row-function needed for painters
+# datasource: the datasource object as defined in plugins/views/datasources.py
+# columns: the list of livestatus columns to query
+# add_columns: list of columns the datasource is known to add itself
+#  (couldn't we get rid of this parameter by looking that up ourselves?)
+# add_headers: additional livestatus headers to add
+# only_sites: list of sites the query is limited to
+# limit: maximum number of data rows to query
+def query_data(datasource, columns, add_columns, add_headers, only_sites = [], limit = None):
+    tablename = datasource["table"]
+    add_headers += datasource.get("add_headers", "")
+    merge_column = datasource.get("merge_by")
+    if merge_column:
+        columns = [merge_column] + columns
+
+    # Most layouts need current state of object in order to
+    # choose background color - even if no painter for state
+    # is selected. Make sure those columns are fetched. This
+    # must not be done for the table 'log' as it cannot correctly
+    # distinguish between service_state and host_state
+    if "log" not in datasource["infos"]:
+        state_columns = []
+        if "service" in datasource["infos"]:
+            state_columns += [ "service_has_been_checked", "service_state" ]
+        if "host" in datasource["infos"]:
+            state_columns += [ "host_has_been_checked", "host_state" ]
+        for c in state_columns:
+            if c not in columns:
+                columns.append(c)
+
+    # Remove columns which are implicitely added by the datasource
+    columns = [ c for c in columns if c not in add_columns ]
+
+    query = "GET %s\n" % tablename
+    query += "Columns: %s\n" % " ".join(columns)
+    query += add_headers
+    live.set_prepend_site(True)
+    if limit != None:
+        live.set_limit(limit + 1) # + 1: We need to know, if limit is exceeded
+
+    if only_sites:
+        live.set_only_sites(only_sites)
+    data = live.query(query)
+    live.set_only_sites(None)
+    live.set_prepend_site(False)
+    live.set_limit() # removes limit
+
+    if merge_column:
+        data = merge_data(data, columns)
+
+    # convert lists-rows into dictionaries.
+    # performance, but makes live much easier later.
+    columns = ["site"] + columns + add_columns
+    rows = [ dict(zip(columns, row)) for row in data ]
+
+    return rows
+
+
+# Merge all data rows with different sites but the same value
+# in merge_column. We require that all column names are prefixed
+# with the tablename. The column with the merge key is required
+# to be the *second* column (right after the site column)
+def merge_data(data, columns):
+    merged = {}
+    mergefuncs = [lambda a,b: ""] # site column is not merged
+
+    def worst_service_state(a, b):
+        if a == 2 or b == 2:
+            return 2
+        else:
+            return max(a, b)
+
+    def worst_host_state(a, b):
+        if a == 1 or b == 1:
+            return 1
+        else:
+            return max(a, b)
+
+    for c in columns:
+        tablename, col = c.split("_", 1)
+        if col.startswith("num_") or col.startswith("members"):
+            mergefunc = lambda a,b: a+b
+        elif col.startswith("worst_service"):
+            return worst_service_state
+        elif col.startswith("worst_host"):
+            return worst_host_state
+        else:
+            mergefunc = lambda a,b: a
+        mergefuncs.append(mergefunc)
+
+    for row in data:
+        mergekey = row[1]
+        if mergekey in merged:
+            oldrow = merged[mergekey]
+            merged[mergekey] = [ f(a,b) for f,a,b in zip(mergefuncs, oldrow, row) ]
+        else:
+            merged[mergekey] = row
+
+    # return all rows sorted according to merge key
+    mergekeys = merged.keys()
+    mergekeys.sort()
+    return [ merged[k] for k in mergekeys ]
+
+
