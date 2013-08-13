645fba9e841a34a56b8c06ce2e5fd73b06e2d0c4
Samuel Milette-Lacombe <samuel.milette-lacombe@savoirfairelinux.com>
2013-08-13 10:20:05 -0400

    Add state_type columns and service_output columns

--- app/model/columns/builtin.py
+++ app/model/columns/builtin.py
@@ -11,6 +11,8 @@ COL_LAST_CHECK = 'last_check'
 COL_CHECK_COMMAND = 'check_command'
 COL_SITE = 'site'
 COL_SERVICE_DESCRIPTION = 'service_description'
+COL_SERVICE_PLUGIN_OUTPUT = 'service_plugin_output'
+COL_STATE_TYPE = 'state_type'
 
 _ = gettext
 painters = {}
@@ -23,11 +25,15 @@ painters[COL_HOST_STATE] = ColumnPainterState(COL_HOST_STATE, _(u'Host state'), 
 nagios_service_states = {0 : 'OK', 1: 'WARNING', 2: 'CRITICAL', 3: 'UNKNOWN'}
 painters[COL_SERVICE_STATE] = ColumnPainterState(COL_SERVICE_STATE, _(u'Service state'), _(u'Service state'), ['services'], nagios_service_states)
 
+nagios_state_type = {0: 'SOFT', 1: 'HARD'}
+painters[COL_STATE_TYPE] = ColumnPainterState(COL_STATE_TYPE, _(u'State type'), _(u'State type'), ['services', 'hostsbygroup', 'servicesbygroup','servicesbyhostgroup', 'log', 'hosts'], nagios_state_type)
+
+
 painters[COL_LAST_CHECK] = ColumnPainterLastEvent(COL_LAST_CHECK, _(u'Last check'), _(u'Last check'), ['hosts', 'services'])
 painters[COL_CHECK_COMMAND] = ColumnPainterRaw(COL_CHECK_COMMAND, _(u'Check command'), _(u'Check command'), ['hosts', 'services'])
 painters[COL_SITE] = ColumnPainterRaw(COL_SITE, _(u'Site'), _(u'Site'), ['hosts', 'services'])
 painters[COL_SERVICE_DESCRIPTION] = ColumnPainterRaw(COL_SERVICE_DESCRIPTION, _(u'Service'), _(u'Service'), ['services'])
-
+painters[COL_SERVICE_PLUGIN_OUTPUT] = ColumnPainterRaw(COL_SERVICE_PLUGIN_OUTPUT, _(u'Service output'), _(u'Service output'), ['services'])
 
 def get_columns_pairs(datasources=None):
     column_pairs = []
