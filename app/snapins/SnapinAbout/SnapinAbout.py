7a142efe298a935db2c63ad5ed53aea29996666d
Samuel Milette-Lacombe <samuel.milette-lacombe@savoirfairelinux.com>
2013-08-16 15:04:14 -0400

    Add licenses headers in snapins files

--- app/snapins/SnapinAbout/SnapinAbout.py
+++ app/snapins/SnapinAbout/SnapinAbout.py
@@ -1,3 +1,23 @@
+#   
+#   Copyright (C) 2013
+#   Savoir-Faire Linux Inc.
+#   
+#   This file is part of Sageo
+#   
+#   Sageo is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   Sageo is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with Sageo.  If not, see <http://www.gnu.org/licenses/>
+
+
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash, Module, current_app 
 from flask.ext.babelex import Domain, gettext, ngettext 
 
@@ -8,7 +28,6 @@ from os import path
 
 class SnapinAbout(SnapinBase):
     def __init__(self):
-        #import ipdb;ipdb.set_trace()
         self.translations_path = path.join(path.dirname(__file__), 'translations')        
         self.mydomain = Domain(self.translations_path)
         _ = self.mydomain.lazy_gettext
