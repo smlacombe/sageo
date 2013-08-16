636f411656a8a86cbdba03b2d268eb33d806897a
Samuel Milette-Lacombe <samuel.milette-lacombe@savoirfairelinux.com>
2013-08-16 12:10:10 -0400

    Add license header in each important file. Delete useless files

--- app/controllers/side.py
+++ app/controllers/side.py
@@ -1,18 +1,20 @@
 #   
 #   Copyright (C) 2013 Savoir-Faire Linux Inc.
 #   
-#   This program is free software: you can redistribute it and/or modify
+#   This file is part of Sageo
+#   
+#   Sageo is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
 #   the Free Software Foundation, either version 3 of the License, or
 #   (at your option) any later version.
 #
-#   This program is distributed in the hope that it will be useful,
+#   Sageo is distributed in the hope that it will be useful,
 #   but WITHOUT ANY WARRANTY; without even the implied warranty of
 #   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #   GNU General Public License for more details.
 #
 #   You should have received a copy of the GNU General Public License
-#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#   along with Sageo.  If not, see <http://www.gnu.org/licenses/>
 
 from flask import Flask, request, session, g, redirect, url_for, \
     abort, render_template, flash, Module, current_app
